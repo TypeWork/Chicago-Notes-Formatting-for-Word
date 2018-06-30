4f6241312d2f743e42a0e9bc397575dce52eba81
Omar Abdool <40541963+TypeWork@users.noreply.github.com>
2018-06-30 16:04:44 -0400

    Updated ChicagoNotesFormatting.vb
    No longer changes the settings for footnotes and endnotes. Instead, macros adapt to user's choice of formatting for footnotes and endnotes.

--- ChicagoNotesFormatting.vb
+++ ChicagoNotesFormatting.vb
@@ -3,52 +3,29 @@ Sub ChicagoNotesFormatting()
     ' If endnotes found in active document...
     If ActiveDocument.Endnotes.Count >= 1 Then
 
-        ' Endnotes are placed at the end of a section and labelled using Arabic numerals
-        With Selection.EndnoteOptions
-            .Location = wdEndOfSection
-            .NumberingRule = wdRestartContinuous
-            .StartingNumber = 1
-            .NumberStyle = wdNoteNumberStyleArabic
-        End With
-
-        ' In the endnotes, endnote marks are followed by a period and are not superscript
+        Set NotesOptions = Selection.EndnoteOptions
         Set NotesRange = ActiveDocument.StoryRanges(wdEndnotesStory)
-        Call MarkFormatting(NotesRange, "\1. ")
-
-
-        If ActiveDocument.Footnotes.Count >= 1 Then
-
-        ' Footnotes are restarted on each and labelled using symbols (without a following period)
-            With Selection.FootnoteOptions
-                .Location = wdBottomOfPage
-                .StartingNumber = 1
-                .NumberStyle = wdNoteNumberStyleSymbol
-                .NumberingRule = wdRestartPage
-            End With
-
-            ' In the footnotes, footnote marks are not superscript nor are they followed by a period
-            Set NotesRange = ActiveDocument.StoryRanges(wdFootnotesStory)
+        
+        ' If endnotes use symbols, a period does not immediately follow endnote marks in the endnotes
+        If NotesOptions.NumberStyle = wdNoteNumberStyleSymbol Then
             Call MarkFormatting(NotesRange, "\1 ")
-
+        Else
+            Call MarkFormatting(NotesRange, "\1. ")
         End If
 
-    ' If no endnotes found in active document...
-    Else
+    End If
 
-        If ActiveDocument.Footnotes.Count >= 1 Then
+    ' If footnotes found in active document...
+    If ActiveDocument.Footnotes.Count >= 1 Then
 
-            ' Footnote count is continuous with footnote marks using Arabic numerals
-            With Selection.FootnoteOptions
-                .Location = wdBottomOfPage
-                .NumberingRule = wdRestartContinuous
-                .StartingNumber = 1
-                .NumberStyle = wdNoteNumberStyleArabic
-            End With
-
-            ' In the footnotes, footnote marks are followed by a period and are not superscript
-            Set NotesRange = ActiveDocument.StoryRanges(wdFootnotesStory)
+        Set NotesOptions = Selection.FootnoteOptions
+        Set NotesRange = ActiveDocument.StoryRanges(wdFootnotesStory)
+        
+        ' If footnotes use symbols, a period does not immediately follow footnote marks in the footnotes
+        If NotesOptions.NumberStyle = wdNoteNumberStyleSymbol Then
+            Call MarkFormatting(NotesRange, "\1 ")
+        Else
             Call MarkFormatting(NotesRange, "\1. ")
-
         End If
 
     End If
@@ -56,15 +33,15 @@ Sub ChicagoNotesFormatting()
 End Sub
 
 
-Sub MarkFormatting(ByRef NotesRange, MyReplaceText As String)
+Sub MarkFormatting(ByRef NotesRange, NoteMarkText As String)
 
     ' Find footnote/endnote marks in specified range (NotesRange).
-    ' With find, superscript formatting removed and note marks typeset based on a specified string pattern (MyReplaceText).
+    ' With find, superscript formatting removed and note marks typeset based on a specified string pattern (NoteMarkText).
     With NotesRange.Find
         .ClearFormatting
         .Text = "(^2)([. ]{1,})"
         .Replacement.ClearFormatting
-        .Replacement.Text = MyReplaceText
+        .Replacement.Text = NoteMarkText
         .Replacement.Font.Superscript = False
         .Forward = True
         .Wrap = wdFindContinue
